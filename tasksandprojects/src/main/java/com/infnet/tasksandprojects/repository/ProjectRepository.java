7d68f64c17dd8bad90d2e6107a902a08e017a5dd
brunorudy1 <brunorudy1@gmail.com>
2024-09-12 22:43:33 -0300

    Removendo códigos para refatoração

--- tasksandprojects/src/main/java/com/infnet/tasksandprojects/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infnet.tasksandprojects.repository;
-
-import com.infnet.tasksandprojects.domain.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
--- tasksandprojects/src/main/java/com/infnet/tasksandprojects/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infnet.tasksandprojects.repository;
-
-import com.infnet.tasksandprojects.domain.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
